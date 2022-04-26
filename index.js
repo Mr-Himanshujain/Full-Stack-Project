4c12f194bc6c496bcc367f7453f7e82e9d0c5283
himanshu jain <ultimatehj8@gmail.com>
2022-04-26 09:51:45 +0530

    first comment

---
+++ index.js
@@ -0,0 +1,11 @@
+setTimeout(() => {
+    console.log("test");
+    console.debug("test")
+}, 3000);
+
+setInterval(() => {
+    // let i=0
+    // i++
+    console.warn('hi')
+    //console.log("hello")
+}, 3000);
