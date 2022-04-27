0cddae294ec2922918b87412e065a6e6cd1137f3
Mr-himanshujain <hj10himanshu@gmail.com>
2022-04-27 09:14:17 +0530

    first comment

--- index.js
+++ index.js
@@ -9,3 +9,14 @@ setInterval(() => {
     console.warn('hi')
     //console.log("hello")
 }, 3000);
+
+// setTimeout(wakeUpUser,5000);
+// function wakeUpUser() {
+//     alert("are u going stare at this boring page forever?");
+// } 
+// var price = 28.99;
+// var discount = 10;
+// var total = (price * (discount/100));
+// if(total>25){
+//     freeShipping();
+// }
