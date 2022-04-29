3f6b729ffded960ba83e283a0d5e3e4f036efd19
Mr-himanshujain <hj10himanshu@gmail.com>
2022-04-29 10:17:25 +0530

    first comment

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-setTimeout(() => {
-    console.log("test");
-    console.debug("test")
-}, 3000);
+// setTimeout(() => {
+//     console.log("test");
+//     console.debug("test")
+// }, 3000);
 
-setInterval(() => {
-    // let i=0
-    // i++
-    console.warn('hi')
-    //console.log("hello")
-}, 3000);
+// setInterval(() => {
+//     let i=0
+//     i++
+//     console.warn('hi')
+//     console.log("hello")
+// }, 3000);
 // let a = 2;
 // typeof(a)
 // console.log(typeof(a));
@@ -29,9 +29,46 @@ setInterval(() => {
 //     freeShipping();
 // }
 
-let a = [10,3,4,1,2];
-let c=a.map((ele)=>ele+10);
-console.log(c);
-let d=c.filter(function(ele){ return ele >12})
-console.log(d);
+// let a = [10,3,4,1,2];
+// let c=a.map((ele)=>ele+10);
+// console.log(c);
+// let d=c.filter(function(ele){ return ele >12})
+// console.log(d);
+
+//let b = [];
+// let b = ["abc","xyz","abcde"];
+// let c=b.pop("abc","xyz");
+// let c = b[2];
+// let c=b.filter((ele,i)=>ele.length === 5).toString();
+// console.log(c)
+
+// let a=['a','b','c','d'];
+// let b=a.slice(1,4);
+// console.log(b);
+
+// let a=['a','b','c','d','a','b','c','d','a','b','c','d','a','b','c','d'];
+// let b=a.slice(4,8);
+// let c=b.slice(1,4);
+// console.log(b);
+// console.log(c);
+
+// let c =["car","bike","cycle"];
+// let d=c.slice(1,3).join(" and ");
+// console.log(d);
+
+// let a = [30,40,50];
+// let b = [2,3,4];
+// let c = b.concat(a);
+// console.log(c);
+// let d = a.concat(b);
+// console.log(d);
+
+// let a = "hi im simon";
+// let b=a.split(" ").filter(val=>val.length === 2).join(" & ");
+// console.log(b);
+
+let a = [[name="himanshu",age=21],[name="lamda",age=10]];
+let b = a.filter(val=>val.length==3);
+console.log(a);
+
 
